fa36b1e45029442c88addaf6933f1198f91b560f
austinjschaefers <austinjschaefers@gmail.com>
2019-03-24 18:32:05 -0400

    Tests for input validation

---
+++ Tests/TestValidateInput.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestValidateInput(unittest.TestCase):
+
+    def test_path_exist(self):
+
+
+    def test_is_input(self):
+
+
+    def test_validate_input_type(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
