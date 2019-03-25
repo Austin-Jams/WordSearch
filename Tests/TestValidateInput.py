00dd7d4895274baf86d4ea900909d6c08bb52bf1
austinjschaefers <austinjschaefers@gmail.com>
2019-03-24 21:17:26 -0400

    Test to validate the input is a string

--- Tests/TestValidateInput.py
+++ Tests/TestValidateInput.py
@@ -1,10 +1,16 @@
 import unittest
+import sys
 from search import validate_input
 
 
 class TestValidateInput(unittest.TestCase):
 
     def test_is_argument_passed(self):
+        with self.assertRaises(Exception):
+            validate_input()
+
+    def test_input_data_type(self):
+        sys.argv[1] = 7
         with self.assertRaises(Exception):
             validate_input()
 
