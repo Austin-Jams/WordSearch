7fce3fd732c9bb625a3f269f5c3361efc7574220
austinjschaefers <austinjschaefers@gmail.com>
2019-03-24 20:45:02 -0400

    Test for no argument passed

--- Tests/TestValidateInput.py
+++ Tests/TestValidateInput.py
@@ -1,15 +1,12 @@
 import unittest
+from search import validate_input
 
 
 class TestValidateInput(unittest.TestCase):
 
-    def test_path_exist(self):
-
-
-    def test_is_input(self):
-
-
-    def test_validate_input_type(self):
+    def test_is_argument_passed(self):
+        with self.assertRaises(Exception):
+            validate_input()
 
 
 if __name__ == '__main__':
