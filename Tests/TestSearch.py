35dd10b4fcb0c60e071bd0bbe3482d89a69b2e5a
austinjschaefers <austinjschaefers@gmail.com>
2019-04-08 17:49:20 -0400

    Test for backward search right.

--- Tests/TestSearch.py
+++ Tests/TestSearch.py
@@ -12,6 +12,11 @@ class TestSearch(unittest.TestCase):
         word = "TIN"
         forward = True
         self.assertTrue(solve.search_right(current_position, board, word, forward))
+        forward = False
+        word = 'NIT'
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        current_position = (3,3)
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
 
     def test_search_up(self):
         return 1
