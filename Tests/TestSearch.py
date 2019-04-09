f2f5c244f5cb690395e4e00db8b4d4929a23a507
austinjschaefers <austinjschaefers@gmail.com>
2019-04-09 19:20:15 -0400

    Test for backward search right. Also limited the number of functions to optimize the program and not have unreachable code.

--- Tests/TestSearch.py
+++ Tests/TestSearch.py
@@ -15,26 +15,43 @@ class TestSearch(unittest.TestCase):
         forward = False
         word = 'NIT'
         board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        current_position = (3,1)
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+
+    def test_search_down(self):
+        current_position = (3, 1)
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        word = "TIN"
+        forward = True
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+        forward = False
+        word = 'NIT'
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        current_position = (3, 3)
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+
+
+    def test_search_down_right(self):
+        current_position = (3, 1)
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        word = "TIN"
+        forward = True
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+        forward = False
+        word = 'NIT'
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        current_position = (3, 3)
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+
+    def test_search_down_left(self):
+        current_position = (3,1)
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
+        word = "TIN"
+        forward = True
+        self.assertTrue(solve.search_right(current_position, board, word, forward))
+        forward = False
+        word = 'NIT'
+        board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
         current_position = (3,3)
         self.assertTrue(solve.search_right(current_position, board, word, forward))
 
-    def test_search_up(self):
-        return 1
-
-    def test_search_down(self):
-        return 1
-
-    def test_search_left(self):
-        return 1
-
-    def test_search_up_right(self):
-        return 1
-
-    def test_search_down_right(self):
-        return 1
-
-    def test_search_down_left(self):
-        return 1
-
-    def test_search_up_left(self):
-        return 1
