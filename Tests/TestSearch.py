bce9e13db392ea56649ad0bc17e1a4491448fe41
austinjschaefers <austinjschaefers@gmail.com>
2019-04-09 19:23:27 -0400

    Code for forward and backward search down.

--- Tests/TestSearch.py
+++ Tests/TestSearch.py
@@ -19,15 +19,15 @@ class TestSearch(unittest.TestCase):
         self.assertTrue(solve.search_right(current_position, board, word, forward))
 
     def test_search_down(self):
-        current_position = (3, 1)
+        current_position = (0, 1)
         board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
-        word = "TIN"
+        word = "FART"
         forward = True
         self.assertTrue(solve.search_right(current_position, board, word, forward))
         forward = False
-        word = 'NIT'
+        word = 'TRAF'
         board = [['D', 'F', 'G', 'R'], ['L', 'A', 'P', 'A'], ['O', 'R', 'A', 'Y'], ['R', 'T', 'I', 'N']]
-        current_position = (3, 3)
+        current_position = (0, 1)
         self.assertTrue(solve.search_right(current_position, board, word, forward))
 
 
