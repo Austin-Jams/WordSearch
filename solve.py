3389244fa42e2a1d8e933624cae7c4557268f917
austinjschaefers <austinjschaefers@gmail.com>
2019-04-09 20:17:01 -0400

    Code for searching down left both forward and backwards.

--- solve.py
+++ solve.py
@@ -50,6 +50,20 @@ def search_down_right(current_position, board, word, forward):
         column += 1
 
 
+def search_down_left(current_position, board, word, forward):
+    if not forward:
+        word = word[::-1]
+    string_check = ''
+    row = current_position[0]
+    column = current_position[1]
+    while word.startswith(string_check):
+        if string_check == word:
+            return True
+        string_check += board[row][column]
+        row += 1
+        column -= 1
+
+
 
 
 
